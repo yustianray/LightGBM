70646fc012ad59875702043ab0e52f7fe12126d9
guolinke <guolin.ke@outlook.com>
2020-03-03 09:36:18 +0800

    force non-constant-hessian in multi-val-bin

--- src/io/dataset.cpp
+++ src/io/dataset.cpp
@@ -1321,7 +1321,7 @@ void Dataset::ConstructHistogramsInner(
     score_t* ordered_gradients, score_t* ordered_hessians,
     TrainingShareStates* share_state, hist_t* hist_data) const {
   if (!share_state->is_colwise) {
-    return ConstructHistogramsMultiVal<use_indices, use_hessian, false>(
+    return ConstructHistogramsMultiVal<use_indices, true, false>(
         data_indices, num_data, gradients, hessians, share_state, hist_data);
   }
   std::vector<int> used_dense_group;
@@ -1405,12 +1405,12 @@ void Dataset::ConstructHistogramsInner(
   global_timer.Stop("Dataset::dense_bin_histogram");
   if (multi_val_groud_id >= 0) {
     if (num_used_dense_group > 0) {
-      ConstructHistogramsMultiVal<use_indices, use_hessian, true>(
+      ConstructHistogramsMultiVal<use_indices, true, true>(
           data_indices, num_data, ptr_ordered_grad, ptr_ordered_hess,
           share_state,
           hist_data + group_bin_boundaries_[multi_val_groud_id] * 2);
     } else {
-      ConstructHistogramsMultiVal<use_indices, use_hessian, false>(
+      ConstructHistogramsMultiVal<use_indices, true, false>(
           data_indices, num_data, gradients, hessians, share_state,
           hist_data + group_bin_boundaries_[multi_val_groud_id] * 2);
     }
